fce46cb86d879ade9227164e2f21d843f1f719d2
slavik <sviatoslavpintsak@gmail.com>
2023-04-15 19:30:57 +0300

    remove unnessary helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-    def urls_to_images(str)
-        str.gsub!(%r{https://[^\s<]+]}) do |url|
-            "<img src='#{url}' width = '100'; />"
-        end
-        str.html_safe
-    end
 end
