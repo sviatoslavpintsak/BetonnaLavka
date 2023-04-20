f025aaf3c2bf5a74932ae3ae0d6b51237fdba1f6
slavik <sviatoslavpintsak@gmail.com>
2023-04-20 11:55:07 +0300

    add prod_price

--- db/migrate/20230407103601_create_carts.rb
+++ db/migrate/20230407103601_create_carts.rb
@@ -1,11 +1,11 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
- # def change
-    #create_table :carts do |t|
-    #  t.integer :user_id
-     # t.string :product_product
-     # t.float :quantity
+  def change
+    create_table :carts do |t|
+      t.integer :user_id
+      t.string :product_product
+      t.float :quantity
 
-     # t.timestamps
-   # end
-  #end
+      t.timestamps
+    end
+  end
 end
