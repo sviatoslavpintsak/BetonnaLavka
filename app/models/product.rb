b700666684e1020a5f0b6b6ab563498a3292e74a
slavik <sviatoslavpintsak@gmail.com>
2023-04-20 20:00:18 +0300

    remove valdation for prod/categ

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,9 @@
 class Product < ApplicationRecord
     validates :price, numericality: {greater_than: 0}
-    validate :valid_category
+   
 
     has_one_attached :pics
-    belongs_to :category
+    #belongs_to :category
 
-    private
 
-  def valid_category
-    errors.add(:category, "is not valid") unless Category.exists?(name: category.name)
-  end
 end
