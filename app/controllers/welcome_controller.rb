04529bcea5ae83d330205f63bcf7c166c5438ee0
slavik <sviatoslavpintsak@gmail.com>
2023-04-20 23:49:21 +0300

    welcome look & pics

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
     def index 
+        @category = Category.all
+        @hide_header = true
         render 'welcome/index'
     end
-
     
 end
 
